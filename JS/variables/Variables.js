d73e4b020d042a57806d5f2c710118ad4b3f6da3
NandiniKarunakar <nandanikarunakar@gmail.com>
2024-04-14 09:35:40 +0530

    Initial commit

---
+++ JS/variables/Variables.js
@@ -0,0 +1,12 @@
+//Q1. What is Variable?
+//A.  Variable can be used to values tempararely in RAM
+
+//Examples:-
+//-------------
+let a = "JavaScript"
+let b = 10
+let c = 'G'
+let d = false;
+let e = 10.9
+
+console.log(a)
